f18d277e5b9354d8cc066ec49c04e13ef5edda8a
Franck Dupont <kyfr59@gmail.com>
2015-02-24 16:22:05 +0100

    First commit (test)

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 // Bootstrap the application.
 require_once 'bootstrap.php';
-
+echo "oo";
 // Configure, initialize, and run the application.
 $application = new Omeka_Application(APPLICATION_ENV);
 $application->getBootstrap()->setOptions(array(
