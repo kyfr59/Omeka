03dae4da283f94fb7e9ee03344bb62516a310881
Franck Dupont <kyfr59@gmail.com>
2015-08-29 11:06:52 +0200

    Fixing bug #157

--- themes/studens/items/search.php
+++ themes/studens/items/search.php
@@ -6,13 +6,13 @@ echo head(array('title' => $pageTitle,
 
 <?php echo drawFil($this->fil); ?>
 
-<h1><?php echo $pageTitle; ?></h1>
+<h1 class="without-subtitle"><?php echo $pageTitle; ?></h1>
 <!--
 <nav class="items-nav navigation secondary-nav">
     <?php echo public_nav_items(); ?>
 </nav>
 -->
-<p class="presentation">Le moteur de recherche vous permet d'accéder aux ressources numériques ainsi qu'aux expositions du portail Studens. Deux choix s'offrent à vous : une recherche simple, plein texte, ou une recherche avancée vous permettant de croiser différents critères.</p>
+<p class="presentation"  style="width:800px; margin: 0 auto;">Le moteur de recherche vous permet d'accéder aux ressources numériques ainsi qu'aux expositions du portail Studens. Deux choix s'offrent à vous : une recherche simple, plein texte, ou une recherche avancée vous permettant de croiser différents critères.</p>
 
 <?php echo $this->partial('items/search-form.php',
     array('formAttributes' =>
